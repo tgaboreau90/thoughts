70aab29c88ff79d753df69b1f49e288ebd506136
Tommy Gaboreau <tgaboreau@local.net>
2016-12-05 14:24:34 +0000

    email.py/tweet.py/test.py replaces thought generation code with call to gen_thought

--- email.py
+++ email.py
@@ -1,30 +1,7 @@
 import random
-ch = random.choice
+from gen_thought import *
 
-recents = [word for word in open('recents.txt', 'r').read().split('\n') if word]
-
-adj = [word for word in open('adjectives.txt', 'r').read().split('\n') if word and word not in recents]
-adv = [word for word in open('adverbs.txt', 'r').read().split('\n') if word and word not in recents]
-verb = [word for word in open('verbs.txt', 'r').read().split('\n') if word and word not in recents]
-noun = [word for word in open('nouns.txt', 'r').read().split('\n') if word and word not in recents]
-
-t_adverb = ch(adv)
-t_verb = ch(verb)
-t_adjective = ch(adj)
-t_noun = ch(noun)
-
-recents.append(t_adverb)
-recents.append(t_verb)
-recents.append(t_adjective)
-recents.append(t_noun)
-
-f = open('recents.txt', 'w')
-for word in recents[-120:]:
-	f.write(word + '\n')
-f.close()
-
-thought = "%s %s %s %s." % (t_adverb, t_verb, t_adjective, t_noun)
-print thought
+thought = gen_thought(exclude = 'recents.txt')
 
 
 import smtplib
