05c24a6cfbe2e0c9011c9c9c86bb7d762e603189
Tommy Gaboreau <tgaboreau@local.net>
2016-12-15 15:03:24 +0000

    deletes obsolete files, renames email.py to send_email.py

--- checklen.py
+++
@@ -1,6 +0,0 @@
-adj = [word for word in open('adjectives.txt', 'r').read().split('\n') if word]
-adv = [word for word in open('adverbs.txt', 'r').read().split('\n') if word]
-verb = [word for word in open('verbs.txt', 'r').read().split('\n') if word]
-noun = [word for word in open('nouns.txt', 'r').read().split('\n') if word]
-
-print max([len(word) for word in adj]) + max([len(word) for word in adv]) + max([len(word) for word in verb]) + max([len(word) for word in noun])
