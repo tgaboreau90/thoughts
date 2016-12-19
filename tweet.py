122c82614c37bba8c15d1e1cb989b56d5d44db91
Ubuntu <ubuntu@example.com>
2016-12-19 10:08:15 +0000

    fixes 2 slight syntax errors in tweet.py

--- tweet.py
+++ tweet.py
@@ -4,12 +4,13 @@ import json
 
 thought = gen_thought(exclude = 'recents.txt')
 
-twitterCreds = json.loads(open(twitterCreds.json, 'r').read())
+twitterCreds = json.loads(open('twitterCreds.json', 'r').read())
 
 token = twitterCreds['token']
-token_key = twitterCreds['token']
+token_key = twitterCreds['token_key']
 con_secret_key = twitterCreds['con_secret_key']
 con_secret = twitterCreds['con_secret']
+
 
 t = Twitter(auth = OAuth(token, token_key, con_secret, con_secret_key))
 
